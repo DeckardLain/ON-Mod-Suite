869826852e4ee77d0cf1e07947a2e03a7d98af55
DeckardLain <syoshimura@hanalani.org>
2018-08-14 14:40:40 -1000

    Update ON Mod Suite.user.js
    Add feature - Switch Roster to Faculty

--- ON Mod Suite.user.js
+++ ON Mod Suite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ON Mod Suite
 // @namespace    http://www.hanalani.org/
-// @version      0.1
+// @version      0.2
 // @description  Collection of mods for Blackbaud ON system
 // @author       Scott Yoshimura
 // @match        https://hanalani.myschoolapp.com/app/*
@@ -16,15 +16,21 @@
 //      Adds links to user pages for quickly switching between module views
 // - People Finder Quick Select
 //      Hit enter when searching with People Finder to open the first result
+// - Switch Roster to Faculty
+//      Class rosters opened in Academics are missing the Send Communication menu.
+//      This adds a link to quickly switch between Faculty and Academics view of the roster.
 
 // Notes:
 // - Currently does not work on Podium pages
+// - Pages won't load when opening links in new tab.  Refresh/Reload will load the page.
+//      "Run at" must be set to document-end?
 
 // ----------------------------------------------------------------------------------------
 // ---------------------------------------Main Section-------------------------------------
 // ----------------------------------------------------------------------------------------
 
 // Check for page hashchanges
+// Borrowed from: https://stackoverflow.com/questions/18989345/how-do-i-reload-a-greasemonkey-script-when-ajax-changes-the-url-without-reloadin
 var fireOnHashChangesToo    = true;
 var pageURLCheckTimer       = setInterval (
     function () {
@@ -42,6 +48,7 @@ var pageURLCheckTimer       = setInterval (
     }
     , 111
 );
+
 
 // Load all mods
 function gmMain(){
@@ -65,12 +72,47 @@ function gmMain(){
             break;
         case "Faculty":
             waitForKeyElements(".bb-page-heading", PostLinkFaculty)
+            break;
+        case "Academics-Roster":
+            waitForKeyElements(".bb-page-heading", PostLinkRosterFaculty)
+            break;
+        case "Faculty-Roster":
+            waitForKeyElements(".bb-page-heading", PostLinkRosterAcademics)
+            break;
     }
 
     // People Finder Quick Select
     waitForKeyElements("#people-finder-search-box", PeopleFinderQuickSelect)
 
 }
+
+
+function GetModule(strURL)
+{
+
+    if (strURL.substring(0, 41).toLowerCase() == "https://hanalani.myschoolapp.com/app/core")
+    {
+        return "Core";
+    } else if (strURL.substring(0, 60) == "https://hanalani.myschoolapp.com/app/academics#academicclass" && strURL.substring(73, 79) == "roster")
+    {
+        return "Academics-Roster";
+    } else if (strURL.substring(0, 58) == "https://hanalani.myschoolapp.com/app/faculty#academicclass" && strURL.substring(71, 77) == "roster")
+    {
+        return "Faculty-Roster";
+    } else if (strURL.substring(0, 46) == "https://hanalani.myschoolapp.com/app/academics")
+    {
+        return "Academics";
+    } else if (strURL.substring(0, 58) == "https://hanalani.myschoolapp.com/app/enrollment-management")
+    {
+        return "Enrollment Management";
+    } else if (strURL.substring(0, 44) == "https://hanalani.myschoolapp.com/app/faculty")
+    {
+        return "Faculty";
+    }
+
+    return;
+}
+
 
 // ----------------------------------------------------------------------------------------
 // -----------------------------------User Module Selector---------------------------------
@@ -165,6 +207,16 @@ function GetLink(strModule, strID)
         case "Faculty":
             strLinkPrefix = "https://hanalani.myschoolapp.com/app/faculty#profile/";
             strLinkSuffix = "/progress";
+            break;
+        case "Faculty-Roster":
+            strLinkPrefix = "https://hanalani.myschoolapp.com/app/faculty#academicclass/";
+            strLinkSuffix = "/0/roster";
+            strModule = "Faculty";
+            break;
+        case "Academics-Roster":
+            strLinkPrefix = "https://hanalani.myschoolapp.com/app/academics#academicclass/";
+            strLinkSuffix = "/0/roster";
+            strModule = "Academics";
     }
 
     var strLinkFull = strHTMLPrefix.concat(strLinkPrefix, strID, strLinkSuffix, strHTMLSuffix, strModule, strHTMLSuffix2);
@@ -188,28 +240,8 @@ function GetID(strURL)
 }
 
 
-function GetModule(strURL)
-{
-
-    if (strURL.substring(0, 41).toLowerCase() == "https://hanalani.myschoolapp.com/app/core")
-    {
-        return "Core";
-    } else if (strURL.substring(0, 46) == "https://hanalani.myschoolapp.com/app/academics")
-    {
-        return "Academics";
-    } else if (strURL.substring(0, 58) == "https://hanalani.myschoolapp.com/app/enrollment-management")
-    {
-        return "Enrollment Management";
-    } else if (strURL.substring(0, 44) == "https://hanalani.myschoolapp.com/app/faculty")
-    {
-        return "Faculty";
-    }
-
-    return;
-}
-
 // ----------------------------------------------------------------------------------------
-// ------------------------------People Finder Quick Select---------------------------------
+// ------------------------------People Finder Quick Select--------------------------------
 // ----------------------------------------------------------------------------------------
 
 function PeopleFinderQuickSelect(jNode)
@@ -219,6 +251,38 @@ function PeopleFinderQuickSelect(jNode)
             $(".pf-user:first").click();
         }
     })
+}
+
+// ----------------------------------------------------------------------------------------
+// ------------------------------Switch Roster to Faculty----------------------------------
+// ----------------------------------------------------------------------------------------
+
+function PostLinkRosterFaculty(jNode)
+{
+    var strURL = window.location.href
+    var strLinks = "Switch to: "
+    var strID = GetID(strURL);
+    if(strID == null || strID.length != 9){
+        return;
+    }
+
+    strLinks = strLinks.concat(GetLink("Faculty-Roster", GetID(strURL)));
+    jNode.after(strLinks);
+    return;
+}
+
+function PostLinkRosterAcademics(jNode)
+{
+    var strURL = window.location.href
+    var strLinks = "Switch to: "
+    var strID = GetID(strURL);
+    if(strID == null || strID.length != 9){
+        return;
+    }
+
+    strLinks = strLinks.concat(GetLink("Academics-Roster", GetID(strURL)));
+    jNode.after(strLinks);
+    return;
 }
 
 // ----------------------------------------------------------------------------------------
